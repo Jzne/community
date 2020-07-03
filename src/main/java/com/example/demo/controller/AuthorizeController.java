52db229f2a21349f604a1432bf18af2ccfc32030
Jzne <Jzness@163.com>
2020-07-03 23:36:26 +0800

    demo commit

--- src/main/java/com/example/demo/controller/AuthorizeController.java
+++ src/main/java/com/example/demo/controller/AuthorizeController.java
@@ -11,7 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
 @Controller
@@ -33,7 +34,7 @@ public class AuthorizeController {
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
                            @RequestParam(name = "state") String state,
-                           HttpServletRequest request) {
+                           HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -44,15 +45,15 @@ public class AuthorizeController {
         GithubUserDTO githubUserDTO = githubProvider.getUser(accessToken);
         if (githubUserDTO != null) {
             User user = new User();
-            user.setToken(UUID.randomUUID().toString());
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
             user.setName(githubUserDTO.getName());
             user.setAccountId(String.valueOf(githubUserDTO.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
-            // 登录成功，写cookie 和session
-            request.getSession().setAttribute("user", githubUserDTO);
-            System.out.println(githubUserDTO.toString());
+            response.addCookie(new Cookie("token", token));
+
             return "redirect:index";
         } else {
             // 登录失败，请重新登录
